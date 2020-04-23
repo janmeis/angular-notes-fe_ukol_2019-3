2b9f11dcec64970a9983399011a773d5adf5b7bc
Jan Meissner <janmeis@yahoo.co.uk>
2020-04-23 18:11:51 +0200

    firebase + environment + app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCD8rLinzZa1tPpy9Cd4Ucu4pHUg-HG_4A',
+    authDomain: 'angular-notes-5df80.firebaseapp.com',
+    databaseURL: 'https://angular-notes-5df80.firebaseio.com',
+    projectId: 'angular-notes-5df80',
+    storageBucket: 'angular-notes-5df80.appspot.com',
+    messagingSenderId: '587170831829',
+    appId: '1:587170831829:web:80f97f8494b8c52ed5b15c',
+    measurementId: 'G-37KRSKNHD7'
+  }
 };
 
 /*

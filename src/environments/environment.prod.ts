9c3e6ce21231f9eea9ad1181c272cc2757268244
Jan Meissner <janmeis@yahoo.co.uk>
2020-04-30 12:27:11 +0200

    app deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCD8rLinzZa1tPpy9Cd4Ucu4pHUg-HG_4A',
+    authDomain: 'angular-notes-5df80.firebaseapp.com',
+    databaseURL: 'https://angular-notes-5df80.firebaseio.com',
+    projectId: 'angular-notes-5df80',
+    storageBucket: 'angular-notes-5df80.appspot.com',
+    messagingSenderId: '587170831829',
+    appId: '1:587170831829:web:80f97f8494b8c52ed5b15c',
+    measurementId: 'G-37KRSKNHD7'
+  }
 };
